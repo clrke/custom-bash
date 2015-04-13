0cdf4051797d9af2eefa3b3bb9a4b41d90a846a8
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-04-13 08:55:00 +0800

    Remove / after .git in gcark

--- custom.bash
+++ custom.bash
@@ -23,7 +23,7 @@ alias gc='git commit -m'
 alias gp='git push'
 alias gclone='git clone'
 gcark() {
-  git clone git@github.com:arkeidolon/$1.git/
+  git clone git@github.com:arkeidolon/$1.git
 }
 
 #Laravel
